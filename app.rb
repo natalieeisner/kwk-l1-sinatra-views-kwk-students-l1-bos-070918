76d98ddea1a76f418815aee0b7c2c70213577295
Natalie Eisner <natalieeisner5@gmail.com>
2018-07-17 17:22:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+configure do
+  set :views, "views"
+  set :public_dir, "public"
+ end
+
 	get '/' do
-		
+		erb :index
 	end
+	
+	get "/info" do
+	 erb :info
+	end
+
 end
+
